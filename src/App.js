edcee2cac473d9c63c231e33b88817ffbdf963eb
sudddy <suddpro@gmail.com>
2022-10-08 11:36:19 +1300

    Base Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router";
+import Home from "./Home";
+import Jobs from "containers/Jobs"
+
+function App() {
+  return (
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/jobs" element={<Jobs />} />
+     </Routes>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+        <div className="App flex  justify-center p-10">
+        <a href="/jobs" className="px-10 py-2 bg-sky-500 border-2 border-sky-500 text-white rounded-sm">View Jobs</a>
+      </div>
+    )
+};
+
+export default Home;
